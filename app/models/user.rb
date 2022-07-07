7a544e90f046e66115a65502804a1bf54ee528e9
kikumon <s.kiku3728@gmail.com>
2022-07-07 13:49:49 +0000

    Finish modeling-user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 100 },
-                    format: { with: VALID_EMAIL_REGEX }
-                    uniqueness:true
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: true
   has_secure_password
-  validates :passeord, presence: true, length: { minimum:6 }
+  validates :password, presence: true, length: { minimum:6 }
 end
