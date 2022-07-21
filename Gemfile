9e7c9d7b324dc8bd6b706a11bb6b841342923a2b
EC2 Default User <s.kiku3728@gmail.com>
2022-07-21 14:06:01 +0000

    Finish japanization-and-time-format

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 
 gem 'rails',        '~> 5.1.6'
+gem 'rails-i18n' # 今回はこのgemを追加しています。
 gem 'bcrypt' # 今回追加するgemです
 gem 'faker' # この行を追加してください。
 gem 'bootstrap-sass'
--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,8 @@ module AttendanceApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
     config.time_zone = 'Asia/Tokyo' # この１行をここに追加します。
-    # Settings in config/environments/* take precedence over those specified here.
+    config.i18n.default_locale = :ja# デフォルトの言語を日本語に設定します。# Settings in config/environments/* take precedence over those specified here.
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
   end
