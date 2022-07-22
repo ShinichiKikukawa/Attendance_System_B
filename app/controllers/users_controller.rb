97c51eaed82a769c082ffed9d705148d2d4add75
EC2 Default User <s.kiku3728@gmail.com>
2022-07-22 14:12:03 +0000

    9.2前

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -47,7 +47,7 @@ class UsersController < ApplicationController
   private
 
     def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+      params.require(:user).permit(:name, :email, :department, :password, :password_confirmation)
     end
 
     # beforeフィルター
