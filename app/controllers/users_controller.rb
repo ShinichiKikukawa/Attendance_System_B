26594ed3cd1bfe0fb00ef664789c0d5bac197b59
EC2 Default User <s.kiku3728@gmail.com>
2022-07-29 18:16:22 +0000

    10.4前

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,14 +3,13 @@ class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy, :edit_basic_info, :update_basic_info]
   before_action :correct_user, only: [:edit, :update]
   before_action :admin_user, only: [:destroy, :edit_basic_info, :update_basic_info]
+  before_action :set_one_month, only: :show
 
   def index
     @users = User.paginate(page: params[:page])
   end
 
   def show
-    @first_day = Date.current.beginning_of_month
-    @last_day = @first_day.end_of_month
   end
 
   def new
@@ -63,12 +62,16 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :email, :department, :password, :password_confirmation)
     end
+    # beforeフィルター
+
+    # paramsハッシュからユーザーを取得します。
+    def set_user
+      @user = User.find(params[:id])
+    end
     
     def basic_info_params
       params.require(:user).permit(:department, :basic_time, :work_time)
     end
-
-    # beforeフィルター
 
     # paramsハッシュからユーザーを取得します。
     def set_user
