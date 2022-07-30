45a4edc13c6e4b403b1e475ed7c981691ea3183f
EC2 Default User <s.kiku3728@gmail.com>
2022-07-30 17:00:50 +0000

    10.4あたり

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,17 +6,19 @@ class ApplicationController < ActionController::Base
 
   # ページ出力前に1ヶ月分のデータの存在を確認・セットします。
   def set_one_month 
-    @first_day = Date.current.beginning_of_month
+    @first_day = params[:date].nil? ?
+    Date.current.beginning_of_month : params[:date].to_date
     @last_day = @first_day.end_of_month
     one_month = [*@first_day..@last_day] # 対象の月の日数を代入します。
     # ユーザーに紐付く一ヶ月分のレコードを検索し取得します。
-    @attendances = @user.attendances.where(worked_on: @first_day..@last_day)
+    @attendances = @user.attendances.where(worked_on: @first_day..@last_day).order(:worked_on)
 
     unless one_month.count == @attendances.count # それぞれの件数（日数）が一致するか評価します。
       ActiveRecord::Base.transaction do # トランザクションを開始します。
         # 繰り返し処理により、1ヶ月分の勤怠データを生成します。
         one_month.each { |day| @user.attendances.create!(worked_on: day) }
       end
+      @attendances = @user.attendances.where(worked_on: @first_day..@last_day).order(:worked_on)
     end
 
   rescue ActiveRecord::RecordInvalid # トランザクションによるエラーの分岐です。
---
+++ app/controllers/attendances_controller.rb
@@ -0,0 +1,16 @@
+class AttendancesController < ApplicationController
+
+  def update
+    @user = User.find(params[:user_id])
+    @attendance = Attendance.find(params[:id])
+    # 出勤時間が未登録であることを判定します。
+    if @attendance.started_at.nil?
+      if @attendance.update_attributes(started_at: Time.current.change(sec: 0))
+        flash[:info] = "おはようございます！"
+      else
+        flash[:danger] = "勤怠登録に失敗しました。やり直してください。"
+      end
+    end
+    redirect_to @user
+  end
+end
