86ece3658c2a972c26daa10cb6130de59167239e
EC2 Default User <s.kiku3728@gmail.com>
2022-08-01 14:25:20 +0000

    Finish add-user-attendances

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,4 +1,5 @@
 class AttendancesController < ApplicationController
+  UPDATE_ERROR_MSG = "勤怠登録に失敗しました。やり直してください。"
 
   def update
     @user = User.find(params[:user_id])
@@ -8,7 +9,13 @@ class AttendancesController < ApplicationController
       if @attendance.update_attributes(started_at: Time.current.change(sec: 0))
         flash[:info] = "おはようございます！"
       else
-        flash[:danger] = "勤怠登録に失敗しました。やり直してください。"
+        flash[:danger] = UPDATE_ERROR_MSG
+      end
+    elsif @attendance.finished_at.nil?
+      if @attendance.update_attributes(finished_at: Time.current.change(sec: 0))
+        flash[:info] = "お疲れ様でした。"
+      else
+        flash[:danger] = UPDATE_ERROR_MSG
       end
     end
     redirect_to @user
