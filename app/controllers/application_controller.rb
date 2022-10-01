3bc35a417dbd2de0c20b850f20981036e0cf9458
EC2 Default User <ec2-user@ip-172-31-10-186.ap-northeast-1.compute.internal>
2022-10-01 19:58:54 +0000

    add-admin_or_correct_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -29,7 +29,15 @@ class ApplicationController < ActionController::Base
   def admin_user
     redirect_to root_url unless current_user.admin?
   end
-
+  
+  def admin_or_correct_user
+    @user = User.find(params[:user_id]) if @user.blank?
+    unless current_user?(@user) || current_user.admin?
+      flash[:danger] = "権限がありません"
+      redirect_to current_user
+    end
+  end
+  
   # ページ出力前に1ヶ月分のデータの存在を確認・セットします。
   def set_one_month 
     @first_day = params[:date].nil? ?
