f17cf16c03e49dd4854d33941c3e791efbe9a150
EC2 Default User <ec2-user@ip-172-31-10-186.ap-northeast-1.compute.internal>
2022-10-10 17:52:49 +0000

    Update Gemfile for Heroku

--- db/schema.rb
+++ db/schema.rb
@@ -32,8 +32,8 @@ ActiveRecord::Schema.define(version: 20220726134046) do
     t.string "remember_digest"
     t.boolean "admin", default: false
     t.string "department"
-    t.datetime "basic_time", default: "2022-10-01 23:00:00"
-    t.datetime "work_time", default: "2022-10-01 22:30:00"
+    t.datetime "basic_time", default: "2022-10-10 23:00:00"
+    t.datetime "work_time", default: "2022-10-10 22:30:00"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
